c12866afe5404eb19098416674048be30b071231
PC <akurbegovi1@etf.unsa.ba>
2022-12-18 19:54:34 +0100

    Urađena klasa DaoFactory

---
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -0,0 +1,32 @@
+package ba.unsa.etf.rpr.dao;
+
+/**
+ * Factory method for singleton implementation of DAOs
+ * @author Adna Kurbegović
+ */
+
+public class DaoFactory {
+    private static final HotelDao hotelDao = new HotelDaoSQLImpl();
+    private static final ReservationDao reservationDao = new ReservationDaoSQLImpl();
+    private static final RoomDao roomDao = new RoomDaoSQLImpl();
+    private static final UserDao userDao = new UserDaoSQLImpl();
+
+    private DaoFactory(){
+    }
+
+    public static HotelDao hotelDao(){
+        return hotelDao;
+    }
+
+    public static ReservationDao reservationDao(){
+        return reservationDao;
+    }
+
+    public static RoomDao roomDao(){
+        return roomDao;
+    }
+
+    public static UserDao UserDao(){
+        return userDao;
+    }
+}
