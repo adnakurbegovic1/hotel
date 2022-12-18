63dd39664afab19b7799c48220f409d4e9334548
PC <akurbegovi1@etf.unsa.ba>
2022-12-18 12:15:03 +0100

    Napravljen interface Dao, vlastiti tip izuzetka HotelException

---
+++ src/main/java/ba/unsa/etf/rpr/exceptions/HotelException.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr.exceptions;
+
+public class HotelException extends Exception{
+
+    public HotelException(String msg, Exception reason){
+        super(msg, reason);
+    }
+
+    public HotelException(String msg){
+        super(msg);
+    }
+}
+
