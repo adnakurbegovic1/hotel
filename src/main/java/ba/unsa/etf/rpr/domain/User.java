4d69c4bc1e6ce7a1d84282a57f801a08f2b5aca4
PC <akurbegovi1@etf.unsa.ba>
2022-12-18 13:10:18 +0100

    Urađena klasa User: getteri, setteri, equals, toString i hashCode metode

--- src/main/java/ba/unsa/etf/rpr/domain/User.java
+++ src/main/java/ba/unsa/etf/rpr/domain/User.java
@@ -1,4 +1,65 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
+
 public class User {
+    private int idUser;
+    private String name;
+    private String surname;
+    private String email;
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "idUser=" + idUser +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return idUser == user.idUser;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idUser, name, surname, email);
+    }
 }
