7da6b29f71a120f75cffa696cb01d3069fa026ea
PC <akurbegovi1@etf.unsa.ba>
2023-02-17 10:16:15 +0100

    goToMain metoda u LoginController

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -1,10 +1,15 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
@@ -12,6 +17,10 @@ import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 public class LoginController {
 
     public Button cancelBtn;
+    public TextField emailFld;
+    public PasswordField passwordFld;
+    public Button btnLogin;
+    User u = new User();
     public void backToHome(ActionEvent actionEvent) {
         try{
             Stage stage = (Stage) cancelBtn.getScene().getWindow();
@@ -27,5 +36,27 @@ public class LoginController {
             e.printStackTrace();
         }
 
+    }
+
+    public void goToMain(ActionEvent event){
+
+
+        try {
+
+            u.setPassword(passwordFld.getText());
+            u.setEmail(emailFld.getText());
+            Stage stage = (Stage) btnLogin.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
+            MainController cont = new MainController();
+            fxmlLoader.setController(cont);
+            stage.setTitle("*");
+            stage.setScene(new Scene(fxmlLoader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.show();
+
+        }
+        catch (Exception e){
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
     }
 }
