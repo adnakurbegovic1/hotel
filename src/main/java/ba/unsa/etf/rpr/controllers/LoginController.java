0864089e6bea05eb2ee7f652a7c6ff3eb35f7ed2
PC <akurbegovi1@etf.unsa.ba>
2023-02-17 10:03:52 +0100

    backToHome metoda u LoginController

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -1,4 +1,31 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class LoginController {
+
+    public Button cancelBtn;
+    public void backToHome(ActionEvent actionEvent) {
+        try{
+            Stage stage = (Stage) cancelBtn.getScene().getWindow();
+            stage.close();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/home.fxml"));
+            fxmlLoader.setController(new HomeController());
+            Parent root = fxmlLoader.load();
+            stage.setTitle("Hotel \"50 zvjezdica\"");
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.show();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
 }
