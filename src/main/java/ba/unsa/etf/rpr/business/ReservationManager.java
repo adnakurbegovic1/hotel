d9f3306efc8d7231073241a2d561d4fbce30e874
PC <akurbegovi1@etf.unsa.ba>
2023-02-20 15:46:42 +0100

    ReservationManager javaDoc

--- src/main/java/ba/unsa/etf/rpr/business/ReservationManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ReservationManager.java
@@ -4,7 +4,14 @@ import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.Reservation;
 import ba.unsa.etf.rpr.exceptions.HotelException;
 
+/**
+ * Business Logic Layer for management of Reservations
+ *
+ * @author Adna Kurbegović
+ */
 public class ReservationManager {
+
+
     public static Reservation addReservation(Reservation reservation) throws HotelException {
         try {
             return DaoFactory.reservationDao().add(reservation);
