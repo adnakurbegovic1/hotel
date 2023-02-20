7f4008d30a276e2075098f26d8083a9c603e3a21
PC <akurbegovi1@etf.unsa.ba>
2023-02-20 20:35:07 +0100

    initialize metoda u MyReservationsController

--- src/main/java/ba/unsa/etf/rpr/controllers/MyReservationsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MyReservationsController.java
@@ -1,16 +1,31 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.dao.ReservationDaoSQLImpl;
+import ba.unsa.etf.rpr.dao.RoomDaoSQLImpl;
+import ba.unsa.etf.rpr.exceptions.HotelException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class MyReservationsController {
     public Button btnBack;
+
+    public ListView listOfReservations;
+
+    private ReservationDaoSQLImpl dao;
+
+    @FXML
+    public void initialize() throws HotelException {
+        dao = ReservationDaoSQLImpl.getInstance();
+        listOfReservations.setItems(dao.myReservations());
+    }
     public void goBack(ActionEvent event){
         try {
             Stage stage = (Stage) btnBack.getScene().getWindow();
