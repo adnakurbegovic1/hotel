552b7ae98fa8dd769bcc9ced2decedf10aa904c4
PC <akurbegovi1@etf.unsa.ba>
2023-02-20 20:17:29 +0100

    goBackToMain metoda

--- src/main/java/ba/unsa/etf/rpr/controllers/MyReservationsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MyReservationsController.java
@@ -1,4 +1,30 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+
 public class MyReservationsController {
+    public Button btnBack;
+    public void goBack(ActionEvent event){
+        try {
+            Stage stage = (Stage) btnBack.getScene().getWindow();
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
+            MainController cont = new MainController();
+            fxmlLoader.setController(cont);
+            stage.setTitle("*****");
+            stage.setScene(new Scene(fxmlLoader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.show();
+        }
+        catch (Exception e){
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }
+    }
+
 }
