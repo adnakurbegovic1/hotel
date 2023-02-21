6048715971a9385a97622ef3879eb03efefe98f7
PC <akurbegovi1@etf.unsa.ba>
2023-02-21 22:48:31 +0100

    Kreirana RoomTest klasa

---
+++ src/test/java/ba/unsa/etf/rpr/domain/RoomTest.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTest {
+
+    @Test
+    void getCapacity() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+}
