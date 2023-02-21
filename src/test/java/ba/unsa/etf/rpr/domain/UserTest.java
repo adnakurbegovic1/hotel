0407866cee8e7729a23f81367dcd266908f14592
PC <akurbegovi1@etf.unsa.ba>
2023-02-21 22:40:45 +0100

    setUser metoda

--- src/test/java/ba/unsa/etf/rpr/domain/UserTest.java
+++ src/test/java/ba/unsa/etf/rpr/domain/UserTest.java
@@ -4,5 +4,14 @@ import org.junit.jupiter.api.BeforeEach;
 
 
 class UserTest {
+    private User user = new User();
 
+    @BeforeEach
+    public void setUser() {
+        user.setId(1);
+        user.setName("Adna");
+        user.setSurname("Kurbegovic");
+        user.setEmail("akurbegovi1@etf.unsa.ba");
+        user.setPassword("adna");
+    }
 }
