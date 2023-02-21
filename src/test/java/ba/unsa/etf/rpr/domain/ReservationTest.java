6a1f3588d15d1a52bb6151aae19cbb588e632b66
PC <akurbegovi1@etf.unsa.ba>
2023-02-21 22:59:33 +0100

    setRoom metoda

--- src/test/java/ba/unsa/etf/rpr/domain/ReservationTest.java
+++ src/test/java/ba/unsa/etf/rpr/domain/ReservationTest.java
@@ -1,7 +1,25 @@
 package ba.unsa.etf.rpr.domain;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import java.time.LocalDate;
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ReservationTest {
+
+    private Reservation r = new Reservation();
+    private User u = new User();
+
+    @BeforeEach
+    public void setRoom() {
+        u.setName("Adna");
+        r.setId(1);
+        r.setArrivalDate(LocalDate.MIN);
+        r.setArrivalDate(LocalDate.MAX);
+        r.setRoomNumber(1);
+        r.setUser(u);
+    }
 
 }
