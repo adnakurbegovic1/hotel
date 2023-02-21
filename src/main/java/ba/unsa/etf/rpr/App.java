5ab55a51d0518137fe77a83face0d3f4248dbd87
PC <akurbegovi1@etf.unsa.ba>
2023-02-21 21:35:40 +0100

    main metoda u App

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,13 +1,87 @@
 package ba.unsa.etf.rpr;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import ba.unsa.etf.rpr.business.UserManager;
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.User;
+import ba.unsa.etf.rpr.exceptions.HotelException;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) throws HotelException {
+
+        System.out.println("Za prijavu upišite P, a za registraciju R: ");
+        Scanner scanner = new Scanner(System.in);
+        String value;
+        value = scanner.next();
+        if (value.equals("P")) {
+            String email;
+            String password;
+            System.out.println("Upišite email: ");
+            Scanner newScaner = new Scanner(System.in);
+            email = newScaner.next();
+            System.out.println("Upišite password: ");
+            password = scanner.next();
+            User user = DaoFactory.userDao().getByEmail(email);
+
+            while (true) {
+                // If login is successful
+                if (user != null && user.getPassword().equals(password)) {
+                    System.out.println("Prijava uspješna!");
+                    break;
+                }
+
+                if (user == null) {
+                    System.out.println("Email nije ispravan. Ponovite unos! ");
+                    String email2;
+                    Scanner Scanner2 = new Scanner(System.in);
+                    email2 = Scanner2.next();
+                }
+
+                if (!user.getPassword().equals(password)) {
+                    System.out.println("Pogrešan password. Ponovite unos! ");
+                    String password2;
+                    Scanner Scanner2 = new Scanner(System.in);
+                    password2 = Scanner2.next();
+                    if (password.equals(user.getPassword())) break;
+                }
+            }
+
+            //  showEmployee(user.getId());
+        }
+
+        if (value.equals("R")) {
+            try {
+                String name;
+                String surname;
+                String email;
+                String password;
+                System.out.println("Unesite ime: ");
+                Scanner enterName = new Scanner(System.in);
+                name = enterName.next();
+                System.out.println("Unesite prezime: ");
+                Scanner enterSurname = new Scanner(System.in);
+                surname = enterSurname.next();
+                System.out.println("Unesite email: ");
+                Scanner enterEmail = new Scanner(System.in);
+                email = enterEmail.next();
+                System.out.println("Unesite password: ");
+                Scanner enterPassword = new Scanner(System.in);
+                password = enterPassword.next();
+
+
+                User u = new User();
+                u.setName(name);
+                u.setSurname(surname);
+                u.setEmail(email);
+                u.setPassword(password);
+                UserManager.registration(u);
+                System.out.println("Uspješna registracija!");
+                // showEmployee(u.getId());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
+
 }
