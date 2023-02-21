9eefd5624b7db76f86dca1d2041a41dad3b82d3e
PC <akurbegovi1@etf.unsa.ba>
2023-02-21 22:50:31 +0100

    setRoom test

--- src/test/java/ba/unsa/etf/rpr/domain/RoomTest.java
+++ src/test/java/ba/unsa/etf/rpr/domain/RoomTest.java
@@ -1,10 +1,20 @@
 package ba.unsa.etf.rpr.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RoomTest {
+
+    private Room room = new Room();
+
+    @BeforeEach
+    public void setRoom() {
+        room.setId(1);
+        room.setCapacity(2);
+        room.setPrice(50);
+    }
 
     @Test
     void getCapacity() {
